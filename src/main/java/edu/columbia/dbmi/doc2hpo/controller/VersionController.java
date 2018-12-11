8b44fc4d0334593a29ad40196804af376ca77a28
Cong Liu <stormliucong@gmail.com>
2018-12-11 16:12:39 -0500

    add version info

---
+++ src/main/java/edu/columbia/dbmi/doc2hpo/controller/VersionController.java
@@ -0,0 +1,29 @@
+package edu.columbia.dbmi.doc2hpo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.columbia.dbmi.doc2hpo.pojo.Version;
+
+
+@Controller
+public class VersionController {
+
+	@RequestMapping("/version")
+	@ResponseBody
+	public Version getVersion() {
+		Version version = new Version();
+		// major version: new function backward incompatible;
+		// minor version: new function backward compatible;
+		// patch: bug corrected;
+		version.setDoc2hpo("0.15.1");
+		version.setJava("1.8.0_191");
+		version.setTomcat("8.5.35");
+		version.setMetamap("2016v2");
+		version.setNcbo(null);
+        
+		return version;
+    }
+
+}
---
+++ src/main/java/edu/columbia/dbmi/doc2hpo/pojo/Version.java
@@ -0,0 +1,42 @@
+package edu.columbia.dbmi.doc2hpo.pojo;
+
+public class Version {
+	
+	public String doc2hpo;
+	public String java;
+	public String tomcat;
+	public String metamap;
+	public String ncbo;
+	
+	public String getDoc2hpo() {
+		return doc2hpo;
+	}
+	public void setDoc2hpo(String doc2hpo) {
+		this.doc2hpo = doc2hpo;
+	}
+	public String getJava() {
+		return java;
+	}
+	public void setJava(String java) {
+		this.java = java;
+	}
+	public String getTomcat() {
+		return tomcat;
+	}
+	public void setTomcat(String tomcat) {
+		this.tomcat = tomcat;
+	}
+	public String getMetamap() {
+		return metamap;
+	}
+	public void setMetamap(String metamap) {
+		this.metamap = metamap;
+	}
+	public String getNcbo() {
+		return ncbo;
+	}
+	public void setNcbo(String ncbo) {
+		this.ncbo = ncbo;
+	}
+
+}
